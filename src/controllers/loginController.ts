60807db7b0555bee1d39448144d506c19fe2b509
Emeka-web-dev <chukwurex042@gmail.com>
2024-10-23 13:30:28 -0700

    feature: add login route

---
+++ src/controllers/loginController.ts
@@ -0,0 +1,79 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { compareSync } from "bcryptjs";
+
+import { StatusError } from "../exceptions/status-error";
+import { LoginSchema } from "../schemas";
+import { getUserByEmail } from "../data/user";
+import { JWT_ACCESS_SECRET, JWT_REFRESH_SECRET } from "../secrets";
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const validatedFields = LoginSchema.safeParse(req.body);
+
+    if (!validatedFields.success) {
+      return next(
+        new StatusError("Unprocessable Entity", 422, validatedFields.error)
+      );
+    }
+    const { email, password } = validatedFields.data;
+
+    const currentUser = await getUserByEmail(email);
+
+    if (!currentUser) {
+      return next(new StatusError("Invalid user", 401));
+    }
+
+    if (!compareSync(password, currentUser.password)) {
+      return next(new StatusError("Invalid Credential", 400));
+    }
+
+    const accessToken = jwt.sign(
+      {
+        userId: currentUser.id,
+      },
+      JWT_ACCESS_SECRET,
+      { expiresIn: "60s" }
+    );
+
+    const newRefreshToken = jwt.sign(
+      {
+        userId: currentUser.id,
+      },
+      JWT_REFRESH_SECRET,
+      { expiresIn: "2h" }
+    );
+
+    if (req.cookies) {
+      res.clearCookie("token", {
+        httpOnly: true,
+        sameSite: "none",
+        secure: process.env.NODE_ENV === "production",
+      });
+    }
+
+    res.cookie("token", newRefreshToken, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 2000, // 2 hour
+    });
+
+    const { email: userEmail, id, password: userPassword } = currentUser;
+    return res.json({
+      userEmail,
+      id,
+      userPassword,
+      accessToken,
+    });
+  } catch (error: any) {
+    if (error.issues) {
+      return next(new StatusError("Unprocessable Entity", 422, error?.issues));
+    }
+    return next(new StatusError("Something went wrong!", 500, error));
+  }
+};
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { signup } from "../controllers/registerController";
-// import { login } from "../controllers/authController";
+import { login } from "../controllers/loginController";
 // import { refresh } from "../controllers/refreshController";
 // import { logout } from "../controllers/logoutController";
-// import { loginLimiter } from "../middlewares/login-limiter";
+import { loginLimiter } from "../middlewares/login-limiter";
 
 const authRouter: Router = Router();
 
 authRouter.post("/signup", signup);
-// authRouter.post("/login", [loginLimiter], login);
+authRouter.post("/login", [loginLimiter], login);
 // authRouter.get("/refresh", refresh);
 // authRouter.get("/logout", logout);
 
